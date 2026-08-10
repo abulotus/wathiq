30c95d1b6e5cf965e987a583fa4a37ed396c0c00
adham <adham_karaker@hotmail.com>
2026-08-10 18:26:53 +0100

    Restore original site fonts

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font -- this locale layout is the app's root document */
 import type { Metadata, Viewport } from 'next';
 import '../globals.css';
 import { LanguageProvider } from '@/contexts/LanguageContext';
@@ -72,6 +73,12 @@ export default async function LocaleLayout({
   return (
     <html lang={locale} dir={dir} suppressHydrationWarning>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&family=Tajawal:wght@400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
         <script dangerouslySetInnerHTML={{
           __html: `
             (function () {
